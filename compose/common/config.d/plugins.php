70e47efec1ec14128be43dcc58e5c4870f8516f2
erleibiazzio <erleibiazzio@gmail.com>
2022-06-22 09:42:41 -0300

    Instancia plugin AbstractValidator

--- compose/common/config.d/plugins.php
+++ compose/common/config.d/plugins.php
@@ -9,5 +9,15 @@ return [
         'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
         'SamplePlugin' => ['namespace' => 'SamplePlugin'],
         'MapasBlame' => ['namespace' => 'MapasBlame'],
+        'AbstractValidator' => [
+          'namespace' => 'AbstractValidator',
+          'config' => [
+               'is_opportunity_managed_handler' => function ($opportunity) {
+                    $opportunityList =  ['820','821','822','823','825','826','827','840','841','842','843'];
+                    return in_array($opportunity->id, $opportunityList) ? true : false;
+                },
+            ]
+        ],
+
     ]
 ];
