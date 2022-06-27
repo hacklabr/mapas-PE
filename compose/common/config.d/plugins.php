c76833f0cb134a8a6a0c813e2e5ea8dc7dfad6b1
erleibiazzio <erleibiazzio@gmail.com>
2022-06-27 09:19:59 -0300

    Configura pligin AbstractValidator Ref.:#1

--- compose/common/config.d/plugins.php
+++ compose/common/config.d/plugins.php
@@ -473,6 +473,15 @@ return [
                     'MULHER PROVEDORA DE FAMÍLIA MONOPARENTAL:' => 1573,
                 ]
             ]
-        ]
+        ],
+        "AbstractValidator" => [
+            "namespace" => "AbstractValidator",
+            "config" => [
+                "is_opportunity_managed_handler" => function ($opportunity) {
+                    $opportunityList =  ['820', '821', '822', '823', '825', '826', '827', '840', '841', '842', '843'];
+                    return in_array($opportunity->id, $opportunityList) ? true : false;
+                },
+            ]
+        ],
     ]
 ];
