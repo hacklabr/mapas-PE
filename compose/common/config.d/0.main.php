58c405e5626a528f9d1775f60295c2c5b79e451f
erleibiazzio <erleibiazzio@gmail.com>
2022-07-04 12:51:15 -0300

    Atualiza SiteName e Site Description

--- compose/common/config.d/0.main.php
+++ compose/common/config.d/0.main.php
@@ -2,8 +2,8 @@
 use MapasCulturais\i;
 
 return [
-    'app.siteName' => env('SITE_NAME', 'Mapas Culturais Base Project'),
-    'app.siteDescription' => i::__("O Mapas Culturais é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades"),
+    'app.siteName' => env('SITE_NAME', 'Mapa Cultural de pernambuco'),
+    'app.siteDescription' => i::__("O Mapa Cultural de Pernambuco é uma plataforma livre, gratuita e colaborativa de mapeamento da Secretaria da Cultura do Estado do Pernambuco e da Fundação do Patrimônio Histórico e Artístico de Pernambuco. O objetivo é traçarmos, juntos, o cenário cultural pernambucano, considerando e permitindo o acesso às informações sobre eventos, programas, espaços e agentes culturais. Pesquise livremente ou cadastre aqui a sua iniciativa."),
 
     'themes.active' => env('ACTIVE_THEME', 'MapasCulturais\Themes\BaseV1'),
 
