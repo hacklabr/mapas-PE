58d629e4e3975d33f1bfefa7507c73221f6ed194
erleibiazzio <erleibiazzio@gmail.com>
2023-01-11 11:27:23 -0300

    Ajusta Ambiente patra executar a criação do certificado SSL via LetsEncript de forma automática no Build

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -6,7 +6,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
 fi
 
 domain=(homologtemp.pe.gov.br)
-email="contato@pe.gov.br" # Adding a valid address is strongly recommended
+email="contato@camacari.pe.gov.br" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 data_path="./docker-data/certbot"
@@ -30,7 +30,7 @@ fi
 echo "### Creating dummy certificate for $domain ..."
 path="/etc/letsencrypt/live/$domain"
 mkdir -p "$data_path/conf/live/$domain"
-docker-compose run --rm --entrypoint "\
+docker-compose -f docker-compose.certbot.yml run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:1024 -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -39,11 +39,11 @@ echo
 
 
 echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
+docker-compose -f docker-compose.certbot.yml up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domain ..."
-docker-compose run --rm --entrypoint "\
+docker-compose -f docker-compose.certbot.yml run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domain && \
   rm -Rf /etc/letsencrypt/archive/$domain && \
   rm -Rf /etc/letsencrypt/renewal/$domain.conf" certbot
@@ -66,7 +66,7 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose run --rm --entrypoint "\
+docker-compose -f docker-compose.certbot.yml run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
@@ -76,5 +76,4 @@ docker-compose run --rm --entrypoint "\
     --force-renewal" certbot
 echo
 
-echo "### Reloading nginx ..."
-docker-compose exec nginx nginx -s reload
+docker-compose -f docker-compose.certbot.yml down
