f38d1b8a0f8c7062bd5b2c15a16060a26b6e5e09
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-05-25 09:16:00 -0300

    modificação nas configurações

--- compose/common/config.d/0.main.php
+++ compose/common/config.d/0.main.php
@@ -16,21 +16,12 @@ return [
     'app.siteName' => 'Mapa Cultural de Pernambuco',
     'app.siteDescription' => "O Mapa Cultural de Pernambuco é uma plataforma livre, gratuita e colaborativa de mapeamento da Secretaria da Cultura do Estado do Pernambuco e da Fundação do Patrimônio Histórico e Artístico de Pernambuco. O objetivo é traçarmos, juntos, o cenário cultural pernambucano, considerando e permitindo o acesso às informações sobre eventos, programas, espaços e agentes culturais. Pesquise livremente ou cadastre aqui a sua iniciativa.",
 
-    'themes.active' => '',
+    'themes.active' => 'MapasPE',
 
     'app.lcode' => env('APP_LCODE', 'pt_BR,es_ES'),
 
     'base.assetUrl' => $base_url . 'assets/',
     'base.url' => $base_url,
-
-    'mailer.protocol' => null, // 'tls',
-    'mailer.port'   => '587',
-
-    'namespaces' => array(
-        'MapasCulturais\Themes' => THEMES_PATH,
-        'MapasCulturais\Themes\BaseV1' => THEMES_PATH . 'BaseV1/',
-        'Subsite' => THEMES_PATH . 'Subsite/',
-    ),
 
     'doctrine.database' => [
         'host'           => env('DB_HOST', 'db'),
