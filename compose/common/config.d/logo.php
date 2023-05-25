2855f95a0e3f1023ef76e4f5a45a460d007e62a0
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-05-25 13:54:13 -0300

    corrige configuração da imagem do logo

--- compose/common/config.d/logo.php
+++ compose/common/config.d/logo.php
@@ -12,10 +12,8 @@ return [
 
     /* 
     Define a url da imagem da logo do site - Substituirá a logo padrão
-
-    ex: `https://mapacultural.com.br/assets/logo.png`
     */
-    'logo.image' => '/protected/application/themes/MapasPE/assets/img/logo-Mapa-PE.jpeg',  /* https://www.mapacultural.pe.gov.br/files/subsite/1/file/203/logo_da_plataforma_-_140_x_60_px-01-2-2a890e0a5e4b746f33eff9e5ee07ccf9.jpg */
+    'logo.image' => 'img/logo-Mapa-PE.jpeg',
 
     /* Esconde o título e subtitulo */
     'logo.hideLabel' => true,
