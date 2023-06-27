c3705183faba3cfc10db2534ce6bc42f9383d345
LimaSystem <wolegario49@gmail.com>
2023-06-27 16:41:37 -0300

    Ajusta favicon no ambiente

--- compose/common/config.d/favicon.php
+++ compose/common/config.d/favicon.php
@@ -1,5 +1,8 @@
 <?php
 return [
+    'favicon-SVG' => 'img/favicon-SVG.svg',
+    'favicon-pe' => 'img/favicon-pe.ico',
+    'favicon-512x512' => 'img/favicon-512x512.png',
     'favicon-192x192' => 'img/favicon-192x192.png',
     'favicon-180x180' => 'img/favicon-180x180.png',
     'favicon-512x512' => 'img/favicon-512x512.png',
