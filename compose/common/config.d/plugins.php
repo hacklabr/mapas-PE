c91e95cff9b2d1226ad3ffff7265a4c05032d40f
erleibiazzio <erleibiazzio@gmail.com>
2023-08-21 15:37:30 -0300

    Configura o plugin Metabase para rodar no ambiente

--- compose/common/config.d/plugins.php
+++ compose/common/config.d/plugins.php
@@ -2,6 +2,7 @@
 
 return [
     'plugins' => [
+        'Metabase' => ['namespace' => 'Metabase'],
         'EvaluationMethodTechnical' => ['namespace' => 'EvaluationMethodTechnical', 'config' => ['step' => 0.1]],
         'EvaluationMethodSimple' => ['namespace' => 'EvaluationMethodSimple'],
         'EvaluationMethodDocumentary' => ['namespace' => 'EvaluationMethodDocumentary'],
