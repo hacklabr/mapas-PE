f9dedc6302a969e3cd21ec8f1c25d969285f7729
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-10-17 11:41:40 -0300

    configuração do tema PE

---
+++ themes/MapasPE/conf-base.php
@@ -0,0 +1,20 @@
+<?php 
+use \MapasCulturais\i;
+
+return [
+    /* Primeira linha do logo configurável */
+    'logo.title' => 'Mapa Cultural',
+
+    /* Segunda linha do logo configurável */
+    'logo.subtitle' => 'de Pernambuco',
+
+ 
+
+    /* 
+    Define a url da imagem da logo do site - Substituirá a logo padrão
+    */
+    'logo.image' =>'img/logo-Mapa-PE.jpeg',
+
+    /* Esconde o título e subtitulo */
+    'logo.hideLabel' => true,
+];
