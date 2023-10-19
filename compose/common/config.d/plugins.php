356605d9bbcf31342dd0eb0ba19f6cc2c5d0e3d9
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-10-19 16:03:42 -0300

    adiciona plugin para gerenciamento de avaliadores em lote

--- compose/common/config.d/plugins.php
+++ compose/common/config.d/plugins.php
@@ -2,6 +2,8 @@
 
 return [
     'plugins' => [
+        'ValuersManagement' => ['namespace' => 'ValuersManagement'],
+        
         'EvaluationMethodTechnical' => ['namespace' => 'EvaluationMethodTechnical', 'config' => ['step' => 0.1]],
         'EvaluationMethodSimple' => ['namespace' => 'EvaluationMethodSimple'],
         'EvaluationMethodDocumentary' => ['namespace' => 'EvaluationMethodDocumentary'],
