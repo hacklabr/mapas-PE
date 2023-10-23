c217ee1efc7de43ae74f66711af60c438ce9c31c
erleibiazzio <erleibiazzio@gmail.com>
2023-10-23 19:14:14 -0300

    Atualiza textos da home Museus

--- docker/common/conf-base-museus.php
+++ docker/common/conf-base-museus.php
@@ -8,7 +8,23 @@ return [
     /* Segunda linha do logo configurável */
     'logo.subtitle' => 'de Pernambuco',
 
+    'logo.image' => env('LOGO_IMAGE', 'img/logotipoMapasMuseus.png'),
+
+    'logo.hideLabel' => env('LOGO_HIDELABEL', true),
  
+    "text:home-header.title" => i::__("Boas-vindas a plataforma Museus de Pernambuco"),
+    "text:home-header.description" => i::__("A plataforma Museus de Pernambuco é uma iniciativa da Secretaria de Cultura do Estado de Pernambuco, por meio da Gerência de Territorialidade e Equipamentos Culturais e o Núcleo de Museus. A plataforma tem como objetivo cadastrar, mapear e identificar em parceria com a sociedade, os museus em atividade em nosso Estado.
+
+    O acesso à plataforma é livre, gratuito e colaborativo permitindo que o cadastro dos museus seja realizado de forma voluntária pelas instituições interessadas. Os dados produzidos pelo sistema poderão ser acessados conforme o interesse do usuário e transformados em fonte de pesquisas ou publicações sobre o cenário museológico estadual."),
+    "text:home-opportunities.description" => i::__("Cadastre-se, participe de editais e oportunidades e concorra aos benefícios."),
+    "text:home-entities.events" => i::__("Você pode pesquisar eventos culturais nos campos de busca do seu interesse que ajudam na  precisão de sua pesquisa. Como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente."),
+    "text:home-entities.agents" => i::__("Neste espaço, estão registrados artistas, gestores e produtores; uma rede de atores envolvidos na cena cultural da região. Você pode cadastrar um ou mais agentes (grupos, coletivos, bandas, instituições, empresas, associações, cooperativas e etc)."),
+    "text:home-entities.projects" => i::__("Aqui você encontra leis de fomento, mostras, convocatórias, chamadas públicas e editais criados, além de diversas iniciativas cadastradas pelos usuários da plataforma."),
+    "text:home-feature.description" => i::__("Confira os últimos destaques de cada uma das áreas."),
+    "text:home-register.title" => i::__("Faça seu cadastro e colabore com a plataforma Museus de Pernambuco"),
+    "text:home-register.description" => i::__("Contribua com a plataforma livre e interativa de mapeamento do cenário cultural e instrumento de governança digital no aprimoramento da gestão pública, dos mecanismos de participação e da democratização do acesso às políticas culturais promovidas pela Secretaria de Cultura do Pará."),
+    'text:home-developers.description' => 'Além disso a plataforma Museus de Pernambuco é um software livre, criado em parceria entre a hacklab/, secretarias de cultura, organizações não governamentais, empresas e coletivos que investem na plataforma. Você pode contribuir para o seu desenvolvimento através do GitHub.',
+    "text:home-map.description" => i::__("Os agentes, os espaços e os eventos cadastrados contam com a geolocalização de seus endereços, encontre-os aqui:"),
 
     /* 
     Define a url da imagem da logo do site - Substituirá a logo padrão
