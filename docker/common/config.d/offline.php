17236104a252945abf0c09c8471d9db4d6c2f140
erleibiazzio <erleibiazzio@gmail.com>
2024-06-11 09:03:20 -0300

    Ajusta redirecionamento para desligar o ambiente para manutenções

---
+++ docker/common/config.d/offline.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'app.offline' => date('Y-m-d H:i:s') > '2024-05-01 08:30:00' && date('Y-m-d H:i:s') < '2024-06-12 09:00:00',
+    'app.offlineUrl' => '/em-breve',
+    'app.offlineBypassFunction' => function() {
+        $senha = $_GET['online'] ?? '';
+        
+        if ($senha === env('OFFLINE_BYPASS')) {
+            $_SESSION['online'] = true;
+        }
+
+        return $_SESSION['online'] ?? false;
+    }
+];
