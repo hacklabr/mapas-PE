c1ddb084fd5b3ec5785293de39df56a55562e385
erleibiazzio <erleibiazzio@gmail.com>
2025-04-05 12:07:15 -0300

    Atualiza rota para a tela de pontos de memoria

--- dev/config.d/routes.php
+++ dev/config.d/routes.php
@@ -4,4 +4,6 @@ $routes = $config['routes'];
 $routes["shortcuts"]['paineis-de-dados'] = ["metabase", "panel"];
 $routes["shortcuts"]['painel-de-dados'] = ["metabase", "dashboard"];
 
+$routes["shortcuts"]['pontos-de-memoria'] = ["search", "memory"];
+
 return ['routes' => $routes];
