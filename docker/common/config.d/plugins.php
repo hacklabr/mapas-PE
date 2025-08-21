9e598b4ce5cba06d7bb413b80f5866c7e7e4a95b
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2025-08-21 16:22:47 -0300

    ativa o plugin AccountStatus

--- docker/common/config.d/plugins.php
+++ docker/common/config.d/plugins.php
@@ -12,6 +12,7 @@ return [
             'skip_user_emails' => ['mapacultural@secult.pe.gov.br'],
             'supportContact' => 'através do 81-31843018 (Whatsapp) e selecione a opção dúvidas técnicas MAPA CULTURAL'
         ],
+        'AccountStatus',
         'RegistrationPayments' => [
             'namespace' => 'RegistrationPayments',
             'config' => [
