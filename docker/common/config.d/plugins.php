7f855eb8bddc1f6eabec7a5c964b3d2d2c98b851
erleibiazzio <erleibiazzio@gmail.com>
2025-12-15 16:13:14 -0300

    Faz com que a config do use a config via variavel de ambiente

--- docker/common/config.d/plugins.php
+++ docker/common/config.d/plugins.php
@@ -16,9 +16,9 @@ return [
         "AccountStatus" => [
             "namespace" => "AccountStatus",
             "config" => [
-                'inactive_seal_id' => 29,
+                'inactive_seal_id' => env('USR_STATUS_INACTIVE_SEAL_ID', 29),
                 'inactive_period' => env('USR_STATUS_INACTIVE_PERIOD', '-1 year'),
-                'updated_seal_id' => 30,
+                'updated_seal_id' => env('USR_STATUS_UPDATED_SEAL_ID', 30),
                 'update_expiration_period' => env('USR_STATUS_LAST_UPDATE', '+1 year'),
                 'update_fields' => [
                     'name',
