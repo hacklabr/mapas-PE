439bd4c815b4b5d057af0c6c044b7a28afda4cb0
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2026-02-02 08:00:44 -0300

    entrypoint customizado

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 set -e
 
+echo "-p /var/www/var/DoctrineProxies /var/www/var/logs"
 mkdir -p /var/www/var/DoctrineProxies /var/www/var/logs
 
+echo "/var/www/var/logs/app.log"
 touch /var/www/var/logs/app.log
 
+echo "chown -R www-data: /var/www/var/DoctrineProxies /var/www/var/logs"
 chown -R www-data: /var/www/var/DoctrineProxies /var/www/var/logs
 
+echo "sudo -E -u www-data /var/www/scripts/db-update.sh"
 sudo -E -u www-data /var/www/scripts/db-update.sh
+
+echo "-E -u www-data /var/www/scripts/mc-db-updates.sh"
 sudo -E -u www-data /var/www/scripts/mc-db-updates.sh
 
 if ! cmp /var/www/version.txt /var/www/var/private-files/deployment-version >/dev/null 2>&1
 then
+    echo "compile sass e generate proxies"
     sudo -E -u www-data /var/www/scripts/compile-sass.sh
     sudo -E -u www-data /var/www/src/tools/doctrine orm:generate-proxies
     cp /var/www/version.txt /var/www/var/private-files/deployment-version
@@ -25,6 +32,7 @@ if [ $BUILD_ASSETS = "1" ]; then
     pnpm run dev
 fi
 
+echo "chown "www-data:www-data /var/www/public/..."
 chown www-data:www-data /var/www/public/assets 
 chown www-data:www-data /var/www/public/files 
 chown www-data:www-data /var/www/var/private-files
@@ -32,6 +40,8 @@ chown www-data:www-data /var/www/var/private-files
 cd /
 touch /nohup.out
 chown www-data: /nohup.out
+
+echo "inicializando crons"
 sudo -E -u www-data nohup /jobs-cron.sh >> /dev/stdout &
 sudo -E -u www-data nohup /recreate-pending-pcache-cron.sh >> /dev/stdout &
 
